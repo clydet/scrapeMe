98e3bd59b7221ab15c8ec142d0da202d26765dc3
clydet <ctedrick@compcc.com>
2015-07-12 11:16:14 -0500

    Delete sendTextAT014.sh

--- sendTextAT014.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl http://textbelt.com/text -d number=6086283938 -d "message=at014 tickets are available!! 8/19/16 mens 4x100m final"
-mail -s "at014 tickets are available! 8/19/16 mens 4x100m final" fdstanford@hotmail.com < message.txt
