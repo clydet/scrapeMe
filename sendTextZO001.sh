6244d2b5eaff24dc6d940aa9061cdf04e359f1c2
clydet <ctedrick@compcc.com>
2015-07-12 11:16:44 -0500

    Delete sendTextZO001.sh

--- sendTextZO001.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl http://textbelt.com/text -d number=6086283938 -d "message=ZO001 tickets are available!! 8/5/16 opening ceremony"
-mail -s "opening ceremony tickets are available! 8/5/16" fdstanford@hotmail.com < message.txt
