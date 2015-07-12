81dff9e37cb8ec1824c3b3b522792187c18f3e34
clydet <ctedrick@compcc.com>
2015-07-12 11:17:30 -0500

    Delete sendTextFB036.sh

--- sendTextFB036.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl http://textbelt.com/text -d number=6086283938 -d "message=fb036 tickets are available!! 8/19/16 womens soccer final"
-mail -s "fb036 tickets are available! 8/19/16 womens soccer final" fdstanford@hotmail.com < message.txt
