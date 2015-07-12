4b8a7d64bade8277ea89e488adbf3177779c8ddb
clydet <ctedrick@compcc.com>
2015-07-12 11:17:01 -0500

    Delete sendTextZC001.sh

--- sendTextZC001.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl http://textbelt.com/text -d number=6086283938 -d "message=ZC001 tickets are available!! 8/21/16 opening ceremony"
-mail -s "closing ceremony tickets are available! 8/21/16" fdstanford@hotmail.com < message.txt
