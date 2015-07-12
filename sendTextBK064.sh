517695bd1e58d5b2a35e02b092b6da899e2c9f80
clydet <ctedrick@compcc.com>
2015-07-12 11:16:24 -0500

    Delete sendTextBK064.sh

--- sendTextBK064.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl http://textbelt.com/text -d number=6086283938 -d "message=bk064 tickets are available!! 8/21/16 mens bb gold medal game"
-mail -s "bk064 tickets are available! 8/21/16" fdstanford@hotmail.com < message.txt
